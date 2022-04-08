4e2576daaea59cb2aa64164f66227efddabafc05
David Busch <buschdr@gmail.com>
2022-04-08 18:15:48 -0500

    Created main.cpp

--- Scheduler/Main.cpp
+++ Scheduler/Main.cpp
@@ -10,10 +10,29 @@ using namespace std;
 int main()
 {
 	bool exit = false;
+	char input = '0';
 
 	while (!exit)
 	{
+		cout << "Select what you would like to do: \n";
+		cout << "Enter 0 to exit: \n";
+		cout << "Enter 1 to add professor: \n";
 
+		cout << endl;
+
+		cin >> input;
+
+		switch (input)
+		{
+			case '0': exit = true;
+				cout << "Exiting...";
+				break;
+
+			case '1': AddProfessor();
+
+			default: cout << "Input error, try again.\n\n";
+				break;
+		}
 	}
 
 	return 0;
